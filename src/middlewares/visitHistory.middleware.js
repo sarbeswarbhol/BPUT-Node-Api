9e0dd0a5202bee10f4acb454681fcf64594d7df7
Sarbeswar Bhol <116259679+sarbeswarbhol@users.noreply.github.com>
2024-09-30 22:34:54 +0530

    fix 2.1

--- src/middlewares/visitHistory.middleware.js
+++ src/middlewares/visitHistory.middleware.js
@@ -2,19 +2,25 @@ import { VisitHistory } from "../models/vistHistory.model.js";
 
 export const trackWebsiteVisit = async (req, res, next) => {
     const ip = req.ip;
+    const route = req.originalUrl;
     console.log(`IP: ${ip}`);
-    console.log(`Request: ${req}`);
+    // console.log(`Request: ${req}`);
 
+    if (route.startsWith("/api/v1/stats") || route.startsWith("/api/v1/auth")) {
+        return next();
+    }
     try {
-        // Save the visit history with IP and timestamp
         const visitHistoryFind = await VisitHistory.findOne({ ip });
+
         if (!visitHistoryFind) {
            const newVisit = new VisitHistory({
             ip,
             timestamp: new Date()
            }) 
-
            await newVisit.save();
+        } else {
+            visitHistoryFind.count += 1;
+            await visitHistoryFind.save()
         }
 
 
--- src/models/vistHistory.model.js
+++ src/models/vistHistory.model.js
@@ -5,6 +5,10 @@ const visitHistorySchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    count: {
+        type: Number,
+        default: 0
+    },
     timestamp: {
         type: Date,
         default: Date.now,
