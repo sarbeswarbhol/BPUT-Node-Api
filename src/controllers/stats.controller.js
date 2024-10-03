b97df52f7aac2d3a376ad1f73736629cd4338008
Sarbeswar Bhol <116259679+sarbeswarbhol@users.noreply.github.com>
2024-10-03 10:01:43 +0530

    Update stats.controller.js

--- src/controllers/stats.controller.js
+++ src/controllers/stats.controller.js
@@ -4,8 +4,81 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { VisitHistory } from "../models/vistHistory.model.js";
 import { ApiRouteAccess } from "../models/routeHistory.model.js";
 
-
 const getAllVisits = asyncHandler(async (req, res) => {
+    try {
+        const totalViewsResult = await VisitHistory.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    totalViews: { $sum: "$count" }
+                }
+            }
+        ]);
+
+        const totalViews = totalViewsResult.length > 0 ? totalViewsResult[0].totalViews : 0;
+
+
+        const visitsByIP = await VisitHistory.aggregate([
+            {
+                $group: {
+                    _id: "$ip",
+                    totalCount: { $sum: "$count" },
+                    firstTimestamp: { $min: "$firsttimestamp" },
+                    lastTimestamp: { $max: "$lasttimestamp" } 
+                }
+            },
+            {
+                $project: {
+                    _id: 0, 
+                    ip: "$_id",
+                    totalCount: 1, 
+                    firstTimestamp: {
+                        $dateToString: {
+                            format: "%Y-%m-%dT%H:%M:%S.%LZ", // Specify the format you want
+                            date: {
+                                $add: ["$firstTimestamp", 19800000] // Add 5 hours 30 minutes in milliseconds
+                            }
+                        }
+                    },
+                    lastTimestamp: {
+                        $dateToString: {
+                            format: "%Y-%m-%dT%H:%M:%S.%LZ", // Specify the format you want
+                            date: {
+                                $add: ["$lastTimestamp", 19800000] // Add 5 hours 30 minutes in milliseconds
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                $sort: { lastTimestamp: -1 } // Sort by lastTimestamp in descending order
+            }
+        ]);
+
+        // Create the response object
+        const response = {
+            wholeView: totalViews,
+            visits: visitsByIP
+        };
+
+        return res
+        .status(200)
+        .json(new ApiResponse(
+         200,
+         {response},
+         "Successfully fetched total views and visits by IP"));
+    } catch (error) {
+        return res
+        .status(500)
+        .json(new ApiResponse(
+        500,
+        {},
+        "Server error while fetching visit history"))
+    }
+});
+
+
+const getTotalViewsAndVisits = asyncHandler(async (req, res) => {
     try {
         const visits = await VisitHistory.aggregate([
             {
